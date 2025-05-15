8daff649e4360da8fa18c233e8ac2bcd84321283
Thiprada17 <Thiprada.mop@dome.tu.ac.th>
2025-05-15 21:58:05 +0700

    edit .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/HiewHub/',
+    base: '/HiewHub-Draft1/',
   plugins: [tailwindcss(), react()],
 });
