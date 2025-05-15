2ad87ad57f9d5fe952351f5c227ca24249e6c2c7
Thiprada17 <Thiprada.mop@dome.tu.ac.th>
2025-05-15 22:23:21 +0700

    edit .

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
 import './App.css';
-import MainRunner from './mainpage/MainRunner.jsx'; 
-import MainFind from './mainpage/MainFind.jsx'; 
+import MainRunner from './mainpage/MainRunner.jsx';
+import MainFind from './mainpage/MainFind.jsx';
 import SignIn from './mainpage/SignIn.jsx';
 import SignUp from './mainpage/SignUp.jsx';
 import PostRunner from './mainpage/PostRunner.jsx';
 import PostFind from './mainpage/PostFind.jsx';
 import EditRunner from './formpage/EditRunner.jsx';
 import EditFind from './formpage/EditFind.jsx';
-import AboutUs from './mainpage/AboutUs.jsx'; 
-import FormFind from './formpage/FormFind.jsx'; 
+import AboutUs from './mainpage/AboutUs.jsx';
+import FormFind from './formpage/FormFind.jsx';
 import FormRunner from './formpage/FormRunner.jsx';
 import Connect from './mainpage/Connect.jsx';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/mainrunner" element={<MainRunner />} />
-        <Route path="/mainfind" element={<MainFind />} />
 
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+    <HashRouter basename="/HiewHub-Draft1">
+      <Router>
+        <Routes>
+          <Route path="/mainrunner" element={<MainRunner />} />
+          <Route path="/mainfind" element={<MainFind />} />
 
-        <Route path="/postrunner" element={<PostRunner />} />
-        <Route path="/postfind" element={<PostFind />} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
 
-        <Route path="/editrunner" element={<EditRunner />} />
-        <Route path="/editfind" element={<EditFind />} />
+          <Route path="/postrunner" element={<PostRunner />} />
+          <Route path="/postfind" element={<PostFind />} />
 
-        <Route path="/formfind" element={<FormFind />} />
-        <Route path="/formrunner" element={<FormRunner />} />
+          <Route path="/editrunner" element={<EditRunner />} />
+          <Route path="/editfind" element={<EditFind />} />
 
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/connect" element={<Connect />} />
-      </Routes>
-    </Router>
+          <Route path="/formfind" element={<FormFind />} />
+          <Route path="/formrunner" element={<FormRunner />} />
+
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/connect" element={<Connect />} />
+        </Routes>
+      </Router>
+
+    </HashRouter>
   );
 }
